6a586e975b60f1897fec07b7a2aba2ca8ccec1a7
Oatile Mdlela <oatile.mdlela@multichoice.co.za>
2025-02-23 10:47:23 +0200

    feat: Implement many-to-many relationship between Students and Modules with Teacher association
    - Added Student, Teacher, Module, and StudentModule entity classes
    - Established many-to-many relationship between Students and Modules using StudentModule join entity
    - Added Teacher entity with properties and relationship to Module
    - Updated ApplicationDbContext to include DbSet properties for Students, Teachers, Modules, and StudentModules
    - Added constructors to initialize entity properties and relationships

---
+++ Models/Module.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UnderservedCommunitiesLearningPlatform.Models
+{
+    public class Module
+    {
+        [Required]
+        [Key]
+        public string? ModuleID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Mark { get; set; }
+
+        [Required]
+        [ForeignKey("Teacher")]
+        public string? TeacherID { get; set; }
+        public Teacher? Teacher { get; set; }
+
+        public ICollection<StudentModule> StudentModules { get; set; }
+
+        public Module()
+        {
+            StudentModules = new List<StudentModule>();
+        }
+
+    }
+}
